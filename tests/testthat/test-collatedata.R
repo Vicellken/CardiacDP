2316140c433960ce38e837c6108e61969e1b088e
Yifei Gu <guyf0601@connect.hku.hk>
2024-02-06 20:16:07 +0800

    update file name; add unit test

---
+++ tests/testthat/test-collatedata.R
@@ -0,0 +1,12 @@
+# Load the necessary library
+library(testthat)
+
+# Define the tests
+test_that("collatedata function tests", {
+    # Test that the function stops if the file does not exist
+    expect_error(collatedata("nonexistent_file.zip"), "File does not exist at the given path.")
+
+    # Test that the function works with a valid zip file
+    test_data <- collatedata("C:/Users/guyf0/Desktop/20210518A.zip")
+    expect_is(test_data, "data.table")
+})
